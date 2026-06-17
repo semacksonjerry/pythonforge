a4c8fc958d0ec89a6c860ddfe607d5abeed4d6b9
CraiggCypher <snypercypher@gmail.com>
2026-06-17 15:49:21 +0000

    Cypher's Assignment on Employee Record System

---
+++ assignments/contribution/cypherassignment.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# Employee Record System
+print("Employee Record System")
+print("----------------------")    
+# Employee Information
+employee_name = input("Enter employee name: ")
+department = input("Enter department: ")
+monthly_salary = input("Enter monthly salary: ")
+                       
+# Display Employee Information
+print(employee_name + " works in the " + department + " department")
+print("Monthly salary: $" + str(monthly_salary))
