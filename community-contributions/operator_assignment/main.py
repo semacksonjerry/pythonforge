23211dddf6b1a4fc7bf4996f64570bd7a73c42d5
semacksonjerry <semacksonjerry2014@gmail.com>
2026-07-01 20:30:55 -0700

    Completed Question 3

---
+++ community-contributions/operator_assignment/main.py
@@ -0,0 +1,17 @@
+num1 = int(input("Enter first number:"))
+
+num2 = int(input("Enter second number:"))
+
+sum_result = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+modulus = num1 % num2
+exponent = num1 ** num2
+
+print("Sum =", sum_result)
+print("Difference =", difference)
+print("Product =", product)
+print("Quotient =", quotient)
+print("Modulus =", modulus)
+print("Exponent =", exponent)
